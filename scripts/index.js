8bbf89649755902a0ec86e753b6bc17861984781
LouisTT93 <Louistripleten@gmail.com>
2025-04-29 23:22:35 -0400

    final-commit

--- scripts/index.js
+++ scripts/index.js
@@ -21,3 +21,38 @@ proAddBtn.addEventListener("click", function () {
 newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+// This is all my java on the form section. Below this line.
+
+const nameInput = document.querySelector(".modal__input-name");
+const descriptionInput = document.querySelector(".modal__input-description");
+
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+
+const profileSubmitBtn = document.querySelector("#post-save-btn");
+
+nameInput.placeholder = profileName.textContent;
+descriptionInput.placeholder = profileDescription.textContent;
+
+profileSubmitBtn.addEventListener("submit", function (event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileDescription.textContent = descriptionInput.value;
+  newPostModal.classList.remove("modal_is-opened");
+});
+
+//
+//
+
+const imageInput = document.querySelector(".modal__input-image");
+const captionInput = document.querySelector(".modal__input-caption");
+
+const postSubmitBtn = document.querySelector("#post-save-btn");
+
+postSubmitBtn.addEventListener("submit", function (event) {
+  event.preventDefault();
+  editProfileModal.classList.add("modal_is-opened");
+});
+console.log("New Post Link:", imageInput.value);
+console.log("New Post Title:", captionInput.value);
