315413cfbc83e7ebb05b6d428b6e7aca277e4ad5
LouisTT93 <Louistripleten@gmail.com>
2025-05-02 11:50:22 -0400

    final-commit-modal-changed

--- scripts/index.js
+++ scripts/index.js
@@ -2,6 +2,27 @@ const proEditBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const modalCloseButton = editProfileModal.querySelector(".modal__close-btn");
 
+const proAddBtn = document.querySelector(".profile__add-btn");
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
+
+const proForm = document.querySelector("#pro-form");
+const nameInput = proForm.querySelector("#card-caption-input");
+const descriptionInput = proForm.querySelector("#profile-description-input");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+
+const postForm = document.querySelector("#post-form");
+const imageInput = postForm.querySelector("#card-image-input");
+const captionInput = postForm.querySelector("#card-description-input");
+
+function logCaption() {
+  let imageInput = postForm.querySelector("#card-image-input").value;
+  console.log("New Post Link:", imageInput);
+  let captionInput = postForm.querySelector("#card-description-input").value;
+  console.log("New Post Title:", captionInput);
+}
+
 proEditBtn.addEventListener("click", function () {
   nameInput.value = profileName.textContent;
   descriptionInput.value = profileDescription.textContent;
@@ -11,10 +32,6 @@ modalCloseButton.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-const proAddBtn = document.querySelector(".profile__add-btn");
-const newPostModal = document.querySelector("#new-post-modal");
-const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
-
 proAddBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
@@ -23,12 +40,6 @@ newPostCloseButton.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
 
-const proForm = document.querySelector("#pro-form");
-const nameInput = proForm.querySelector("#card-caption-input");
-const descriptionInput = proForm.querySelector("#profile-description-input");
-const profileName = document.querySelector(".profile__title");
-const profileDescription = document.querySelector(".profile__subtitle");
-
 proForm.addEventListener("submit", function (event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
@@ -36,13 +47,8 @@ proForm.addEventListener("submit", function (event) {
   editProfileModal.classList.remove("modal_is-opened");
 });
 
-const postForm = document.querySelector("#post-form");
-const imageInput = document.querySelector("#card-image-input");
-const captionInput = document.querySelector("#card-description-inpu");
-
 postForm.addEventListener("submit", function (event) {
   event.preventDefault();
-  console.log("New Post Link:", imageInput);
-  console.log("New Post Title:", captionInput);
+  logCaption(postForm);
   newPostModal.classList.remove("modal_is-opened");
 });
